f075c4e48b7835432db48a0674241c0a8ed8af37
Marcin Matłacz <martin.matlacz@gmail.com>
2026-09-06 20:26:03 +0200

    build: add TranslationCore to host-independent package

--- native-ios/Package.swift
+++ native-ios/Package.swift
@@ -2,9 +2,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "WhatsAppBridgeCore",
+    name: "WhatsAppTranslatorCore",
     products: [
         .library(name: "WhatsAppBridgeCore", targets: ["WhatsAppBridgeCore"]),
+        .library(name: "TranslationCore", targets: ["TranslationCore"]),
     ],
     targets: [
         .target(
@@ -12,15 +13,32 @@ let package = Package(
             path: "WhatsAppTranslator",
             exclude: [
                 "DiagnosticsView.swift",
+                "TranslationCore.swift",
                 "WhatsAppTranslatorApp.swift",
                 "WhatsAppWebProbeView.swift",
             ],
             sources: ["WhatsAppBridgeSupport.swift"]
         ),
+        .target(
+            name: "TranslationCore",
+            path: "WhatsAppTranslator",
+            exclude: [
+                "DiagnosticsView.swift",
+                "WhatsAppBridgeSupport.swift",
+                "WhatsAppTranslatorApp.swift",
+                "WhatsAppWebProbeView.swift",
+            ],
+            sources: ["TranslationCore.swift"]
+        ),
         .testTarget(
             name: "WhatsAppBridgeCoreTests",
             dependencies: ["WhatsAppBridgeCore"],
             path: "WhatsAppTranslatorTests"
+        ),
+        .testTarget(
+            name: "TranslationCoreTests",
+            dependencies: ["TranslationCore"],
+            path: "TranslationCoreTests"
         ),
     ]
 )
