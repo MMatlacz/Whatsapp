83c4bc5dca1a292ec60fb789fabb38f050a9d677
Marcin Matłacz <martin.matlacz@gmail.com>
2026-09-07 08:46:57 +0200

    build: add host-independent persistence target

--- native-ios/Package.swift
+++ native-ios/Package.swift
@@ -7,13 +7,24 @@ let package = Package(
         .library(name: "WhatsAppDomainCore", targets: ["WhatsAppDomainCore"]),
         .library(name: "WhatsAppBridgeCore", targets: ["WhatsAppBridgeCore"]),
         .library(name: "TranslationCore", targets: ["TranslationCore"]),
+        .library(name: "PersistenceCore", targets: ["PersistenceCore"]),
     ],
     targets: [
+        .systemLibrary(
+            name: "CSQLite",
+            path: "CSQLite",
+            pkgConfig: "sqlite3",
+            providers: [
+                .apt(["libsqlite3-dev"]),
+                .brew(["sqlite3"]),
+            ]
+        ),
         .target(
             name: "WhatsAppDomainCore",
             path: "WhatsAppTranslator",
             exclude: [
                 "DiagnosticsView.swift",
+                "SQLitePersistence.swift",
                 "TranslationCore.swift",
                 "WhatsAppBridge.js",
                 "WhatsAppBridgeSupport.swift",
@@ -32,6 +43,7 @@ let package = Package(
             path: "WhatsAppTranslator",
             exclude: [
                 "DiagnosticsView.swift",
+                "SQLitePersistence.swift",
                 "WhatsAppBridge.js",
                 "WhatsAppDomain.swift",
                 "TranslationCore.swift",
@@ -51,6 +63,7 @@ let package = Package(
             path: "WhatsAppTranslator",
             exclude: [
                 "DiagnosticsView.swift",
+                "SQLitePersistence.swift",
                 "WhatsAppBridge.js",
                 "WhatsAppBridgeSupport.swift",
                 "WhatsAppDomain.swift",
@@ -62,6 +75,25 @@ let package = Package(
                 "WhatsAppWebTransportCore.swift",
             ],
             sources: ["TranslationCore.swift"]
+        ),
+        .target(
+            name: "PersistenceCore",
+            dependencies: ["WhatsAppDomainCore", "CSQLite"],
+            path: "WhatsAppTranslator",
+            exclude: [
+                "DiagnosticsView.swift",
+                "TranslationCore.swift",
+                "WhatsAppBridge.js",
+                "WhatsAppBridgeSupport.swift",
+                "WhatsAppDomain.swift",
+                "WhatsAppTransportCore.swift",
+                "WhatsAppTransportDomainMapper.swift",
+                "WhatsAppTranslatorApp.swift",
+                "WhatsAppWebKitBridgeRuntime.swift",
+                "WhatsAppWebProbeView.swift",
+                "WhatsAppWebTransportCore.swift",
+            ],
+            sources: ["SQLitePersistence.swift"]
         ),
         .testTarget(
             name: "WhatsAppDomainCoreTests",
@@ -77,6 +109,11 @@ let package = Package(
             name: "TranslationCoreTests",
             dependencies: ["TranslationCore"],
             path: "TranslationCoreTests"
+        ),
+        .testTarget(
+            name: "PersistenceCoreTests",
+            dependencies: ["PersistenceCore", "WhatsAppDomainCore", "CSQLite"],
+            path: "PersistenceCoreTests"
         ),
     ]
 )
